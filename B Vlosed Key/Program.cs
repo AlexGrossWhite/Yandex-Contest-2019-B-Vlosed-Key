3f7691960256dec70feafea4b4563e0a19fb03a5
AlexGrossWhite <112899421+AlexGrossWhite@users.noreply.github.com>
2023-10-17 19:54:05 +0300

    Initial commit

---
+++ B Vlosed Key/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] openKey = Console.ReadLine().Split(" ").Select(o => Int32.Parse(o)).ToArray();
+int result = 0;
+if (openKey.Max() % openKey.Min() == 0)
+{
+    int a = openKey.Max() / openKey.Min();
+    for (int i = 1; i <= a; i++)
+    {
+        if (a % i == 0 && i % (a / i) == 0) result++;
+    }
+}
+Console.WriteLine((result * 2).ToString());
